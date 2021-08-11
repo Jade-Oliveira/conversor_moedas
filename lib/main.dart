6ae7b9c8a1ffc48300c97aaead406782c4288eff
Jade Oliveira - JOI <jade.oliveira@iteris.com.br>
2021-08-11 14:14:09 -0300

    Conversor de moedas com consumo de API finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,16 @@ const request = 'https://api.hgbrasil.com/finance/quotations?key=394163bc';
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
@@ -20,6 +30,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double? dolar;
+  double? euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +62,7 @@ class _HomeState extends State<Home> {
                     style: TextStyle(color: Colors.amber, fontSize: 25),
                     textAlign: TextAlign.center),
               );
-              //caso tenha erro ele retorna o texto abaixo
+            //caso tenha erro ele retorna o texto abaixo
             default:
               if (snapshot.hasError) {
                 return Center(
@@ -54,14 +71,67 @@ class _HomeState extends State<Home> {
                       textAlign: TextAlign.center),
                 );
               } else {
-                return Container(
-                  color: Colors.green,
+                dolar = snapshot.data!['results']['currencies']['USD']['buy'];
+                euro = snapshot.data!['results']['currencies']['EUR']['buy'];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150,
+                        color: Colors.amber,
+                      ),
+                      buildTextField('Reais', 'R\$', realController, _realChanged),
+                      Divider(),
+                      buildTextField('Dólares', 'US\$', dolarController, _dolarChanged),
+                      Divider(),
+                      buildTextField('Euros', '€', euroController, _euroChanged),
+                    ],
+                  ),
                 );
               }
           }
         },
       ),
     );
+  }
+  //funções que vão verificar as mudanças que fazemos nos campos das moedas
+  void _realChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real/dolar!).toStringAsFixed(2);
+    euroController.text = (real/euro!).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar!).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar! / euro!).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro!).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro! / dolar!).toStringAsFixed(2);
+  }
+
+  void _clearAll() {
+    realController.text = '';
+    dolarController.text = '';
+    euroController.text = '';
   }
 }
 
@@ -74,3 +144,21 @@ Future<Map> getData() async {
   //pega um arquivo json e tranforma num mapa
   return json.decode(response.body);
 }
+
+//função que vai criar meu campo de text utilizando a label e o prefix e retorna o widget
+Widget buildTextField(String label, String prefix, TextEditingController c, Function(String) f) {
+  return TextField(
+    onChanged: f,
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    keyboardType: TextInputType.number,
+  );
+}
+
+
+
